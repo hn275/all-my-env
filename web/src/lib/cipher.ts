25fce7523e527b0adc75a87b094ed2e878ed545c
Hal <haln_01@proton.me>
2023-06-07 01:33:19 -0700

    chore: connected to BE

--- web/src/lib/cipher.ts
+++ web/src/lib/cipher.ts
@@ -6,38 +6,83 @@ import CryptoJS from "crypto-js";
 const test_secret = "hSchE34mgOHmvsQokZGSZY4jPQbqD9qY";
 
 export enum SecretKeys {
-	Auth = "auth",
+  Auth = "auth",
 }
 
 export class Cipher {
-	private _key: string = "";
-	private value: string = "";
+  private _key: string = "";
+  private value: string = "";
 
-	constructor(value: string) {
-		this.value = value;
-	}
+  constructor(value: string) {
+    this.value = value;
+  }
 
-	public setKey(k: SecretKeys) {
-		this._key = this.getKey(k);
-		return this;
-	}
+  public setKey(k: SecretKeys) {
+    this._key = this.getKey(k);
+    return this;
+  }
 
-	public encrypt(): string {
-		if (this._key === "") throw new Error("secret key not set");
-		return aes.encrypt(this.value, this._key).toString();
-	}
+  public async encrypt() {
+    if (this._key === "") throw new Error("secret key not set");
+    const encoder = new TextEncoder();
 
-	public decrypt(): string {
-		if (this._key === "") throw new Error("secret key not set");
-		return aes.decrypt(this.value, this._key).toString(CryptoJS.enc.Utf8);
-	}
+    const iv = crypto.getRandomValues(new Uint8Array(12));
+    const encoded = encoder.encode(this.value);
+    const key = await crypto.subtle.importKey(
+      "raw",
+      encoder.encode(this._key),
+      { name: "AES-GCM" },
+      false,
+      ["encrypt"],
+    );
+    const cipherText = await crypto.subtle.encrypt(
+      { name: "AES-GCM", iv },
+      key,
+      encoded,
+    );
 
-	private getKey(key: SecretKeys): string {
-		switch (key) {
-			case SecretKeys.Auth:
-				return test_secret; // TODO: load from env instead
-			default:
-				return "";
-		}
-	}
+    const decoder = new TextDecoder();
+    return {
+      encrypted: decoder.decode(cipherText),
+      iv,
+    };
+
+    //return aes.encrypt(this.value, this._key).toString();
+  }
+
+  public async decrypt(iv: Uint8Array) {
+    if (this._key === "") throw new Error("secret key not set");
+
+    const encoder = new TextEncoder();
+    const key = await crypto.subtle.importKey(
+      "raw",
+      encoder.encode(this._key),
+      "AES-GCM",
+      false,
+      ["encrypt"],
+    );
+
+    try {
+      const pt = await crypto.subtle.decrypt(
+        { name: "AES-GCM", iv },
+        key,
+        encoder.encode(this.value),
+      );
+
+      const decoder = new TextDecoder();
+      return decoder.decode(pt);
+      //return aes.decrypt(this.value, this._key).toString(CryptoJS.enc.Utf8);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  private getKey(key: SecretKeys): string {
+    switch (key) {
+      case SecretKeys.Auth:
+        return test_secret; // TODO: load from env instead
+      default:
+        return "";
+    }
+  }
 }
