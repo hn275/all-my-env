459aea0f05c391c4882fdc9ccee0b16dc49a26f8
hal <haln_01@proton.me>
2023-06-07 17:14:00 -0700

    feat[wip]: verify auth token

--- server/src/features/auth/verify.rs
+++ server/src/features/auth/verify.rs
@@ -1,13 +1,38 @@
 use actix_web;
 use actix_web::{web, HttpResponse};
-use serde::Deserialize;
+use reqwest;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct Token {
     pub code: String,
 }
 
 pub async fn verify_code(token: web::Json<Token>) -> impl actix_web::Responder {
-    dbg!("{}", &token.code);
+    let t = get_access_token(&token.into_inner()).await.unwrap();
+    dbg!("{}", &t);
     return HttpResponse::Ok();
+}
+
+async fn get_access_token(code: &Token) -> Result<String, ()> {
+    let client = reqwest::Client::new();
+    let response = client.post("https://github.com/login/oauth/access_token");
+    let access_token = response
+        .json(&code)
+        .header("Content-Type", "application/json")
+        .send()
+        .await
+        .map_err(|err| {
+            eprintln!("request failed {}", err);
+            return ();
+        })?
+        //.json::<Token>()
+        .text()
+        .await
+        .map_err(|err| {
+            eprintln!("struct marshal failed {}", err);
+            return ();
+        })?;
+
+    Ok(access_token)
 }
