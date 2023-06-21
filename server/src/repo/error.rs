e9df42043cf25d46d1ab5b45e6bda025f4e93151
Hal <haln_01@proton.me>
2023-06-20 23:56:42 -0700

    feat: associate functions for api error

--- server/src/repo/error.rs
+++ server/src/repo/error.rs
@@ -1,26 +1,71 @@
-use actix_web::{body, http, HttpResponse, HttpResponseBuilder, ResponseError};
+use actix_web::{
+    body,
+    http::{self, StatusCode},
+    HttpResponse, HttpResponseBuilder, ResponseError,
+};
 use serde;
 
 #[derive(Debug)]
 pub struct ApiError {
-    pub code: http::StatusCode,
+    pub code: StatusCode,
     pub message: Option<String>,
 }
 
 impl ApiError {
-    pub fn new(code: http::StatusCode, message: Option<String>) -> ApiError {
+    pub fn new(code: StatusCode, message: Option<String>) -> ApiError {
         return ApiError { code, message };
+    }
+
+    #[allow(unused)] // TODO: remove this flag when on use
+    pub fn bad_request(message: &str) -> ApiError {
+        return ApiError {
+            code: StatusCode::BAD_GATEWAY,
+            message: Some(message.to_string()),
+        };
+    }
+
+    #[allow(unused)] // TODO: remove this flag when on use
+    pub fn not_found(message: Option<String>) -> ApiError {
+        return ApiError {
+            code: StatusCode::NOT_FOUND,
+            message,
+        };
+    }
+
+    #[allow(unused)] // TODO: remove this flag when on use
+    pub fn forbidden() -> ApiError {
+        return ApiError {
+            code: StatusCode::FORBIDDEN,
+            message: None,
+        };
+    }
+
+    pub fn unauthorized(message: Option<String>) -> ApiError {
+        return ApiError {
+            code: StatusCode::UNAUTHORIZED,
+            message,
+        };
+    }
+
+    pub fn internal(err: Box<dyn std::error::Error>) -> ApiError {
+        eprintln!("{}", err.to_string());
+        return ApiError {
+            code: StatusCode::INTERNAL_SERVER_ERROR,
+            message: None,
+        };
+    }
+
+    pub fn bad_gateway(message: String) -> ApiError {
+        return ApiError {
+            code: StatusCode::INTERNAL_SERVER_ERROR,
+            message: Some(message),
+        };
     }
 }
 
 impl std::fmt::Display for ApiError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let message: String = match &self.message {
-            None => String::new(),
-            Some(err) => err.to_owned(),
-        };
-
-        write!(f, "code: {}\nmessage: {}\n", self.code, message)
+        write!(f, "code: {}", &self.code)
     }
 }
 
@@ -30,7 +75,7 @@ struct ErrorMessage {
 }
 
 impl ResponseError for ApiError {
-    fn status_code(&self) -> http::StatusCode {
+    fn status_code(&self) -> StatusCode {
         self.code
     }
 
