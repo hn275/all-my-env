82264036d42acdace13242664a4e255018f14b5c
Hal <haln_01@proton.me>
2023-06-25 00:15:00 -0700

    feat: dynamic key generation with KDF

--- server/src/repo/crypto/keygen.rs
+++ server/src/repo/crypto/keygen.rs
@@ -1,23 +1,36 @@
-use hkdf;
+use hkdf::{self, Hkdf, InvalidLength};
 use sha2::Sha256;
-use std::env::var;
 
-pub fn generate(components: &[String]) -> Result<[u8; 32], hkdf::InvalidLength> {
-    let hkdf_secret = var("HKDF_SECRET").expect("`HKDF_SECRET` not set");
-    let mut c: Vec<String> = Vec::from([hkdf_secret]);
-    for i in components {
-        c.push(i.to_owned());
+pub enum KeyType {
+    RowKey,
+}
+
+impl KeyType {
+    fn get_key(&self) -> String {
+        match self {
+            Self::RowKey => get_env("ROW_KEY_SECRET"),
+        }
     }
+}
 
-    let buf = components
-        .into_iter()
-        .map(|c| c.as_bytes())
-        .collect::<Vec<&[u8]>>()
-        .concat();
+fn get_env(key: &str) -> String {
+    use std::env::var;
+    var(key)
+        .expect(format!("`{}` not set", key).as_str())
+        .to_owned()
+}
 
-    let kdf = hkdf::Hkdf::<Sha256>::new(None, &buf);
-    let mut output_key = [0u8; 32]; // hex encode: 16 -> 32 len
-    kdf.expand(&[], &mut output_key)?;
+pub trait KeyComponent {
+    fn to_bytes(&self) -> Vec<u8>;
+}
 
-    Ok(output_key)
+pub fn generate(key: KeyType, buf: &impl KeyComponent) -> Result<[u8; 32], InvalidLength> {
+    let salt = None;
+    let kdf = Hkdf::<Sha256>::new(salt, &buf.to_bytes());
+
+    let secret = key.get_key().as_bytes().to_owned();
+    let mut output_key = [0u8; 32];
+    kdf.expand(&secret, &mut output_key)?;
+
+    return Ok(output_key);
 }
