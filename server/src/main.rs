425e9fef741c4c150f3fc2510bd137b19f0f6c74
Hal <haln_01@proton.me>
2023-06-25 16:59:11 -0700

    feat: cipher functions

--- server/src/main.rs
+++ server/src/main.rs
@@ -31,6 +31,8 @@ async fn main() -> std::io::Result<()> {
 }
 
 // example
+// NOTE: base64 encoding would be more size efficient,
+// just doing hex encoding here bc going fast
 #[derive(Debug)]
 struct Foo {
     bar: String,
@@ -38,57 +40,69 @@ struct Foo {
     nonce: Option<String>,
 }
 
-impl repo::crypto::keygen::CipherComponent for Foo {
-    fn key(&self) -> Vec<u8> {
-        self.bar.as_bytes().to_owned()
+impl repo::crypto::cipher::CipherComponent for Foo {
+    fn input_key(&self) -> &[u8] {
+        self.bar.as_bytes()
     }
 
-    fn data(&self) -> &[u8] {
+    fn input_data(&self) -> &[u8] {
         self.cipher.as_bytes()
     }
 
-    fn seal(&mut self, c: repo::crypto::keygen::CipherData) {
-        self.cipher = hex::encode(c.ciphertext);
-        self.nonce = Some(hex::encode(c.nonce));
-    }
-
-    fn open(&self) -> repo::crypto::keygen::CipherData {
+    fn open(&self) -> repo::crypto::cipher::CipherData {
         if let None = self.nonce {
             panic!("alskfjklsdjf");
         }
 
         let nonce = &self.nonce.as_ref().unwrap();
-        repo::crypto::keygen::CipherData {
+        repo::crypto::cipher::CipherData {
             nonce: hex::decode(nonce).unwrap().try_into().unwrap(),
             ciphertext: hex::decode(&self.cipher).unwrap(),
         }
+    }
+
+    fn nonce(&self) -> Option<Vec<u8>> {
+        match &self.nonce {
+            None => None,
+            Some(k) => {
+                let nonce = hex::decode(k).unwrap();
+                Some(nonce)
+            }
+        }
+    }
+
+    fn update(&mut self, d: repo::crypto::cipher::CipherData) {
+        self.cipher = hex::encode(d.ciphertext);
+        self.nonce = Some(hex::encode(d.nonce));
     }
 }
 
 #[actix_web::get("/test")]
 async fn test_route() -> impl actix_web::Responder {
-    use repo::crypto::keygen;
+    use repo::crypto::cipher;
     let mut foo = Foo {
         bar: "bar".to_owned(),
         cipher: "baz".to_owned(),
         nonce: None,
     };
 
-    dbg!("before seal: {}", &foo);
+    println!("before seal call");
+    dbg!(&foo);
 
-    keygen::CipherKey::new(keygen::KeyType::RowKey, &foo)
-        .unwrap()
+    cipher::Cipher::new(cipher::KeyType::RowKey)
         .seal(&mut foo)
         .unwrap();
 
-    dbg!("after seal: {}", &foo);
+    println!("after seal call");
+    dbg!(&foo);
 
-    keygen::CipherKey::new(keygen::KeyType::RowKey, &foo)
-        .unwrap()
-        .open(&mut foo)
+    let decrypted = cipher::Cipher::new(cipher::KeyType::RowKey)
+        .open(&foo)
         .unwrap();
 
-    dbg!("after open: {}", &foo);
+    println!("after open");
+    dbg!(&foo);
+    dbg!(std::str::from_utf8(&decrypted).unwrap());
 
     return std::str::from_utf8(&foo.bar.as_ref()).unwrap().to_owned();
 }
