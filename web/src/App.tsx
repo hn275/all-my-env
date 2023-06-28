331c1a94bdea4070583feded053510225e199a43
Hal <haln_01@proton.me>
2023-06-28 00:36:35 -0700

    feat: connect page boilerplate

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home";
+import { Home } from "pages/home";
+import { Connect } from "pages/connect";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route index path="/" element={<Home />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route index path="/" element={<Home />} />
+			<Route index path="/connect" element={<Connect />} />
+		</Routes>
+	);
 }
