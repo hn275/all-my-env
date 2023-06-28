b72cfbfac50d66aa86b3697fa2dee0c600cbcbe8
hal <haln_01@proton.me>
2023-06-28 15:36:42 -0700

    feat: global fonts

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -3,18 +3,18 @@ import Logo from "assets/logo.svg";
 import { Layout } from "components/Layout";
 
 export function Home() {
-  return (
-    <Layout>
-      <img src={Logo} alt="logo" />
-      <a href={getLoginUrl()}>login</a>
-    </Layout>
-  );
+	return (
+		<Layout>
+			<img src={Logo} alt="logo" />
+			<a href={getLoginUrl()}>login</a>
+		</Layout>
+	);
 }
 
 function getLoginUrl(): string {
-  const githubLogin = "https://github.com/login/oauth/authorize";
-  const param = new URLSearchParams({
-    client_id: GITHUB_SECRET,
-  });
-  return githubLogin + "?" + param.toString();
+	const githubLogin = "https://github.com/login/oauth/authorize";
+	const param = new URLSearchParams({
+		client_id: GITHUB_SECRET,
+	});
+	return githubLogin + "?" + param.toString();
 }
