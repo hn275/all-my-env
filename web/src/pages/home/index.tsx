71a13a657d21e9ccae2a2848f6241709dc6cc41f
Hal <haln_01@proton.me>
2023-06-28 12:05:22 -0700

    chore(wip): project set up

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -1,17 +1,20 @@
 import { GITHUB_SECRET } from "lib/github";
+import Logo from "assets/logo.svg";
+import { Layout } from "components/Layout";
 
 export function Home() {
-	return (
-		<>
-			<a href={getLoginUrl()}>login</a>
-		</>
-	);
+  return (
+    <Layout>
+      <img src={Logo} alt="logo" />
+      <a href={getLoginUrl()}>login</a>
+    </Layout>
+  );
 }
 
 function getLoginUrl(): string {
-	const githubLogin = "https://github.com/login/oauth/authorize";
-	const param = new URLSearchParams({
-		client_id: GITHUB_SECRET,
-	});
-	return githubLogin + "?" + param.toString();
+  const githubLogin = "https://github.com/login/oauth/authorize";
+  const param = new URLSearchParams({
+    client_id: GITHUB_SECRET,
+  });
+  return githubLogin + "?" + param.toString();
 }
