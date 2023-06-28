3d33b83764155e8112b2afb9c677cf9e9290d01b
hal <haln_01@proton.me>
2023-06-27 18:30:43 -0700

    chore: react ui

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,24 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{svelte,ts,js,html,astro}"],
-	theme: {
-		extend: {
-			fontFamily: {
-				main: ["Monomaniac One", "sans-serif"],
-				accent: ["Exo 2", "sans-serif"],
-				regular: ["Cabin", "sans-serif"],
-				mono: ["JetBrains Mono", "monospace"],
-			},
-			colors: {
-				main: "#0d9488",
-				dark: "#262626",
-				light: "#d4d4d4",
-				accent: {
-					blue: "#1D4ED8",
-					fuchsia: "#A21CAF",
-				},
-			},
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{ts,tsx,js,jsx,html}"],
+  theme: {
+    extend: {
+      fontFamily: {},
+      colors: {
+        main: "#0d9488",
+        dark: "#262626",
+        light: "#d4d4d4",
+        accent: {
+          blue: "#1D4ED8",
+          fuchsia: "#A21CAF",
+        },
+      },
+    },
+  },
+  plugins: [],
 };
