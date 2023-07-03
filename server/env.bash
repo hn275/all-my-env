c9fa2d8413051824c4e459052522952a0ecfd306
Hal <haln_01@proton.me>
2023-07-03 15:13:46 -0700

    feat: env.bash test scripts

--- server/env.bash
+++ server/env.bash
@@ -2,3 +2,16 @@ export GITHUB_CLIENT_SECRET="asldkfjsadklfjk"
 export GITHUB_CLIENT_ID="asldkfjsadklfjk"
 export ROW_KEY_SECRET="asdflkjasldkfjasldkjfasldkjfasas"
 export JWT_SECRET="asdflkjasldkfjasldkjfasldkjfasas"
+
+function test() {
+    if [ -z "${1}" ];then
+        echo "Testing all packages"
+        # go test $(go list ./... | grep -v lib) -coverprofile cover.out
+        go test ./... -coverprofile cover.out
+    else
+        echo "Testing package ${1}"
+        go test $1 -coverprofile cover.out
+    fi
+    
+    [[ -f cover.out ]] && rm cover.out
+}
