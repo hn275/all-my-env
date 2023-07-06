4b43a83aa550aca67639478612bfe1341185ee01
Hal <haln_01@proton.me>
2023-07-06 01:02:00 -0700

    fix: exporting empty env var only

--- server/env.bash
+++ server/env.bash
@@ -1,13 +1,14 @@
-export GITHUB_CLIENT_SECRET="asldkfjsadklfjk"
-export GITHUB_CLIENT_ID="asldkfjsadklfjk"
-export ROW_KEY_SECRET="asdflkjasldkfjasldkjfasldkjfasas"
-export JWT_SECRET="asdflkjasldkfjasldkjfasldkjfasas"
-export POSTGRES_PASSWORD="password"
-export POSTGRES_USER="username"
-export POSTGRES_DB="envhub"
-export POSTGRES_PORT="5432"
-export POSTGRES_HOST="localhost"
-export POSTGRES_SSLMODE="disable"
+[[ -z ${GITHUB_CLIENT_SECRET} ]] && export GITHUB_CLIENT_SECRET="asldkfjsadklfjk"
+[[ -z ${GITHUB_CLIENT_ID} ]] && export GITHUB_CLIENT_ID="asldkfjsadklfjk"
+[[ -z ${ROW_KEY_SECRET} ]] && export ROW_KEY_SECRET="asdflkjasldkfjasldkjfasldkjfasas"
+[[ -z ${JWT_SECRET} ]] && export JWT_SECRET="asdflkjasldkfjasldkjfasldkjfasas"
+[[ -z ${POSTGRES_PASSWORD} ]] && export POSTGRES_PASSWORD="password"
+[[ -z ${POSTGRES_USER} ]] && export POSTGRES_USER="username"
+[[ -z ${POSTGRES_DB} ]] && export POSTGRES_DB="envhub"
+[[ -z ${POSTGRES_PORT} ]] && export POSTGRES_PORT="5432"
+[[ -z ${POSTGRES_HOST} ]] && export POSTGRES_HOST="localhost"
+[[ -z ${POSTGRES_SSLMODE} ]] && export POSTGRES_SSLMODE="disable"
+
 export POSTGRES_DSN="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?sslmode=${POSTGRES_SSLMODE}"
 
 MIGRATION_DIR="./db/migrations"
@@ -37,4 +38,8 @@ function dbdown() {
 
 function dbfix() {
     migrate -database $POSTGRES_DSN -path ./db/migrations force $1
+}
+
+function dbview() {
+    docker exec -it envhub-db psql -U username envhub
 }
