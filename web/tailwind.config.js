0121db659c7edf861f0fbd4fc977f688824126b2
Hal <haln_01@proton.me>
2023-07-15 11:46:48 -0700

    feat: hero section done

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,22 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{ts,tsx,js,jsx,html}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        accent: ["Alegreya SC", "serifs"],
-        regular: ["Inter", "sans-serifs"],
-      },
-      colors: {
-        main: "#0d9488",
-        dark: "#262626",
-        light: "#d4d4d4",
-        accent: {
-          blue: "#1D4ED8",
-          fuchsia: "#A21CAF",
-        },
-      },
-    },
-  },
-  plugins: [],
+	content: ["./src/**/*.{ts,tsx,js,jsx,html}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				accent: ["Alegreya SC", "serifs"],
+				regular: ["Inter", "sans-serifs"],
+			},
+			colors: {
+				main: "#0d9488",
+				dark: "#262626",
+				light: "#d4d4d4",
+				accent: {
+					blue: "#1D4ED8",
+					fuchsia: {
+						100: "#E879F9",
+						200: "#A21CAF",
+					},
+				},
+			},
+		},
+	},
+	plugins: [],
 };
