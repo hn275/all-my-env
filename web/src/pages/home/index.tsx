eeb8350666c737f26d7e72db522870822f989880
Hal <haln_01@proton.me>
2023-07-15 00:01:45 -0700

    feat: navbar done

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -3,17 +3,17 @@ import { Layout } from "components/Layout";
 import { Nav } from "./Nav";
 
 export function Home() {
-	return (
-		<Layout>
-			<Nav url={getLoginUrl()} />
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Nav oauthUrl={getLoginUrl()} githubUrl="" />
+    </Layout>
+  );
 }
 
 function getLoginUrl(): string {
-	const githubLogin = "https://github.com/login/oauth/authorize";
-	const param = new URLSearchParams({
-		client_id: GITHUB_SECRET,
-	});
-	return githubLogin + "?" + param.toString();
+  const githubLogin = "https://github.com/login/oauth/authorize";
+  const param = new URLSearchParams({
+    client_id: GITHUB_SECRET,
+  });
+  return githubLogin + "?" + param.toString();
 }
