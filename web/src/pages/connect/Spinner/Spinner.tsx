77d9bcf748a21b9c914276748640b7bba52730ca
Hal <haln_01@proton.me>
2023-07-14 23:35:22 -0700

    feat: Nav mobile

--- web/src/pages/connect/Spinner/Spinner.tsx
+++ web/src/pages/connect/Spinner/Spinner.tsx
@@ -1,8 +1,8 @@
 import "./spinner.css";
 export function Spinner() {
-	return (
-		<svg viewBox="25 25 50 50">
-			<circle r="20" cy="50" cx="50" />
-		</svg>
-	);
+  return (
+    <svg viewBox="25 25 50 50" id="spinner">
+      <circle r="20" cy="50" cx="50" />
+    </svg>
+  );
 }
