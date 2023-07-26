c70e561fdf499ca33cdc73b426100800ee73a168
Hal <haln_01@proton.me>
2023-07-26 10:54:31 -0700

    fix: use waitgroup instead of polling

--- server/handlers/repos/variables/variables.go
+++ server/handlers/repos/variables/variables.go
@@ -1,8 +1,11 @@
 package variables
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
+	"os"
+	"sync"
 
 	"github.com/hn275/envhub/server/db"
 	"github.com/hn275/envhub/server/gh"
@@ -15,7 +18,9 @@ type variableHandler struct {
 }
 
 var (
-	Handlers *variableHandler
+	Handlers           *variableHandler
+	errNotAContributor = errors.New("not a contributor")
+	errBadGateWay      = errors.New("GitHub responded an with error")
 )
 
 func init() {
@@ -24,26 +29,28 @@ func init() {
 
 // Endpoint:
 // https://docs.github.com/en/rest/collaborators/collaborators?apiVersion=2022-11-28#check-if-a-user-is-a-repository-collaborator
-func getRepoAccess(c chan<- permission, repoURL string, u *jwt.GithubUser) {
-	g := gh.New(u.Token)
-	res, err := g.Get("/repos/%s/collaborators/%s", repoURL, u.Login)
+func getRepoAccess(c chan<- error, wg *sync.WaitGroup, repoURL string, u *jwt.GithubUser) {
+	wg.Add(1)
+	defer wg.Done()
+
+	res, err := gh.New(u.Token).Get("/repos/%s/collaborators/%s", repoURL, u.Login)
 	if err != nil {
-		buf := permission{false, err}
-		c <- buf
+		c <- err
 		return
 	}
 
 	switch res.StatusCode {
 	case http.StatusNoContent:
-		c <- permission{true, nil}
+		c <- nil
 		return
 
 	case http.StatusNotFound:
-		c <- permission{false, nil}
+		c <- errNotAContributor
 		return
 
 	default:
-		c <- permission{false, fmt.Errorf("Github API reponses [%v]", res.Status)}
+		fmt.Fprintf(os.Stderr, "GitHub API responded with %d", res.StatusCode)
+		c <- errBadGateWay
 		return
 	}
 }
