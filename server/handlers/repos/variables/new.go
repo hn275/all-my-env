18c93ed4bc7a8578e26c66ee6a8f75da53dbaa46
Hal <haln_01@proton.me>
2023-07-31 01:39:12 -0700

    chore: added request body doc

--- server/handlers/repos/variables/new.go
+++ server/handlers/repos/variables/new.go
@@ -14,6 +14,7 @@ import (
 	"gorm.io/gorm"
 )
 
+// request body: { key: string, value: string }
 func (d *variableHandler) NewVariable(w http.ResponseWriter, r *http.Request) {
 	// VALIDATE REQUEST
 	if r.Method != http.MethodPost {
