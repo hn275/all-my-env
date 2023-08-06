1577d17a6e5569e7b342568f1083a1a95fe7cbe9
Hal <haln_01@proton.me>
2023-08-05 17:25:15 -0700

    chore: refactor routing

---
+++ server/handlers/router.go
@@ -0,0 +1,65 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/hn275/envhub/server/handlers/auth"
+	"github.com/hn275/envhub/server/handlers/repos"
+	"github.com/hn275/envhub/server/handlers/repos/variables"
+)
+
+func New() *chi.Mux {
+	r := chi.NewMux()
+
+	// CONFIG
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// CORS
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{
+			"http://localhost:3000",
+			"http://localhost:3000/",
+			"http://127.0.0.1:3000",
+			"http://127.0.0.1:3000/",
+		},
+		AllowedMethods: []string{
+			http.MethodGet,
+			http.MethodPost,
+			http.MethodOptions,
+			http.MethodPut,
+			http.MethodPatch,
+			http.MethodDelete,
+		},
+		AllowedHeaders: []string{
+			"Authorization",
+			"Accept",
+			"Content-Type",
+		},
+	}))
+
+	// ROUTES
+	r.Route("/auth", func(r chi.Router) {
+		r.Handle("/github", http.HandlerFunc(auth.Handler.VerifyToken))
+	})
+
+	r.Route("/repo", func(r chi.Router) {
+		r.Handle("/", http.HandlerFunc(repos.Index))
+		r.Handle("/link", http.HandlerFunc(repos.Link))
+
+		r.Route("/{repoID}", func(r chi.Router) {
+			r.Route("/variables", func(r chi.Router) {
+				r.Handle("/", http.HandlerFunc(variables.Index))
+				r.Group(func(r chi.Router) {
+					r.Use(variables.WriteAccessChecker)
+					r.Handle("/new", http.HandlerFunc(variables.NewVariable))
+				})
+			})
+		})
+	})
+
+	return r
+}
--- server/main.go
+++ server/main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
 	"github.com/hn275/envhub/server/database"
-	"github.com/hn275/envhub/server/handlers/auth"
-	"github.com/hn275/envhub/server/handlers/repos"
-	"github.com/hn275/envhub/server/handlers/repos/variables"
+	"github.com/hn275/envhub/server/handlers"
 )
 
 var port string
@@ -24,55 +20,21 @@ func init() {
 }
 
 func main() {
-	r := chi.NewMux()
-	routerConfig(r)
-
 	// refresh variable counter every second
 	go database.RefreshVariableCounter()
 
-	/* ROUTERS */
-	r.Route("/auth", func(r chi.Router) {
-		r.Handle("/github", http.HandlerFunc(auth.Handler.VerifyToken))
-	})
+	fmt.Print(
+		`
+ _______   ________   ___      ___ ___  ___  ___  ___  ________     
+|\  ___ \ |\   ___  \|\  \    /  /|\  \|\  \|\  \|\  \|\   __  \    
+\ \   __/|\ \  \\ \  \ \  \  /  / \ \  \\\  \ \  \\\  \ \  \|\ /_   
+ \ \  \_|/_\ \  \\ \  \ \  \/  / / \ \   __  \ \  \\\  \ \   __  \  
+  \ \  \_|\ \ \  \\ \  \ \    / /   \ \  \ \  \ \  \\\  \ \  \|\  \ 
+   \ \_______\ \__\\ \__\ \__/ /     \ \__\ \__\ \_______\ \_______\
+    \|_______|\|__| \|__|\|__|/       \|__|\|__|\|_______|\|_______|
+`)
 
-	r.Route("/repo", func(r chi.Router) {
-		r.Handle("/", http.HandlerFunc(repos.Index))
-		r.Handle("/link", http.HandlerFunc(repos.Link))
-
-		r.Route("/{repoID}", func(r chi.Router) {
-			r.Route("/variables", func(r chi.Router) {
-				r.Handle("/", http.HandlerFunc(variables.Index))
-				r.Handle("/new", http.HandlerFunc(variables.NewVariable))
-			})
-		})
-	})
-
+	r := handlers.New()
 	log.Println("Listening on port:", port)
 	log.Fatal(http.ListenAndServe(":"+port, r))
-}
-
-func routerConfig(r *chi.Mux) {
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins: []string{
-			"http://localhost:3000",
-			"http://localhost:3000/",
-			"http://127.0.0.1:3000",
-			"http://127.0.0.1:3000/",
-		},
-		AllowedMethods: []string{
-			http.MethodGet,
-			http.MethodPost,
-			http.MethodOptions,
-			http.MethodPut,
-			http.MethodPatch,
-			http.MethodDelete,
-		},
-		AllowedHeaders: []string{
-			"Authorization",
-			"Accept",
-			"Content-Type",
-		},
-	}))
 }
