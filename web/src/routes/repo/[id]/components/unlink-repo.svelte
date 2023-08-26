1f1a91deae9075c4de4cc412fe85a361d4ed1b3b
Hal <haln_01@proton.me>
2023-08-25 23:53:20 -0700

    feat: unlink repo ui

---
+++ web/src/routes/repo/[id]/components/unlink-repo.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import cx from "classnames";
+</script>
+
+<button
+	class={cx([
+		"btn btn-outline border-error/20 text-error/50 hover:text-error",
+		"hover:border-error/80 hover:bg-dark-200 text-xs",
+	])}
+>
+	<i class="fa-solid fa-link-slash" />
+	Delete repository
+</button>
